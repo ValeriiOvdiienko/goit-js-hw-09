a7ec67ae4d39e790187c7a9e8a6c7d644c4df2d5
ValeriiOvd <vallerii2002@gmail.com>
2026-08-21 01:34:53 +0300

    add js code to form

---
+++ src/js/2-form.js
@@ -0,0 +1,43 @@
+const StorageKey = 'feedback-form-state';
+const formData = { email: '', message: '' };
+console.log(formData);
+
+const form = document.querySelector('.feedback-form');
+
+form.addEventListener('submit', handlerSubmit);
+function handlerSubmit(event) {
+  event.preventDefault();
+  if (!formData.email || !formData.message) {
+    alert('Please fill all fields');
+    return;
+  }
+  localStorage.removeItem(StorageKey);
+  form.reset();
+  formData.email = '';
+  formData.message = '';
+}
+form.addEventListener('input', handlerFormInput);
+
+function handlerFormInput(event) {
+  formData[event.target.name] = event.target.value;
+  localStorage.setItem(StorageKey, JSON.stringify(formData));
+}
+
+populateForm();
+
+function populateForm() {
+  const savedData = localStorage.getItem(StorageKey);
+  if (!savedData) {
+    return;
+  }
+  const parsedData = JSON.parse(savedData);
+  console.log(parsedData);
+  formData.email = parsedData.email || '';
+  formData.message = parsedData.message || '';
+  if (formData.email) {
+    form.elements.email.value = formData.email;
+  }
+  if (formData.message) {
+    form.elements.message.value = formData.message;
+  }
+}
